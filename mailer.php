158299d5ffb5499d6c94580f79aa800ea39a07da
kareem El-Gisuhy <kareem.64123@gmail.com>
2020-10-07 09:55:56 +0200

    getting the f*** mailing service working finally

---
+++ mailer.php
@@ -0,0 +1,45 @@
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
+require 'vendor/autoload.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.gmail.com';
+    $mail->SMTPAuth   = true;                                 
+    $mail->Username   = ''; 
+    $mail->Password   = '';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port       = 587;
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        )
+        );                           
+
+    //Recipients
+    $mail->setFrom('ssco813@gmail.com', 'sseco');
+    $mail->addAddress('ahmedsalem.64123@gmail.com');
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
